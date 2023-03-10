ac1b0adbcf3ee058702975b1b2370316e1d6f8e4
Ramiro Sanchez <ramirosanchezaranda@gmail.com>
2023-03-10 13:28:44 -0300

    1er commit

---
+++ src/app/authentication/components/login/login.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoginComponent } from './login.component';
+
+describe('Pruebas unitarias de autenticacion', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('componente creado correctamente', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Campos correctos', () =>{
+    const form = component.formularioLogin;
+    const usuario = form.controls["usuario"];
+    const contraseña = form.controls["contraseña"];
+
+    usuario.setValue('maxPower');
+    contraseña.setValue('maxpower5690');
+
+    expect(usuario.valid).toBeTrue();
+    expect(contraseña.valid).toBeTrue();
+  })
+});
